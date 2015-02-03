5df5faa9b8cbbea4a26662e708a301cd7a8ca489
dca123 <dsenanayaka1@gmail.com>
2015-02-03 07:35:52 +0530

    Almost done php

--- mail.php
+++ mail.php
@@ -1,10 +1,12 @@
-<?php $name = $_POST['contact-name'];
+<?php
+$name = $_POST['contact-name'];
 $email = $_POST['contact-email'];
-$message = $_POST['contact-message'];
-$formcontent="From: $name \n Message: $message";
+$organi = $_POST['contact-organi'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n \n Organisation: $organi Message: $message";
 $recipient = "dsenanayaka1@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
