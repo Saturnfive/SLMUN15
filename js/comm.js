f56e08b6be908529482e8e000de81b1ea0917ada
dca123 <dsenanayaka1@gmail.com>
2015-03-01 19:45:13 +0530

    Added NavBar

--- js/comm.js
+++ js/comm.js
@@ -2,9 +2,7 @@ $(document).ready(function () {
 	$('.ga1').mouseenter(function (e) {
 			$(this).addClass("zoom");
 			$(this).removeClass("unzoom");
-			$(this).children('h3').fadeOut(200);
-			$(this).children('h4').fadeOut(200);
-			$(this).children('span').fadeIn(500);
+
 		})
 		.mouseleave(function (e) {
 			$(this).addClass("unzoom");
