c59e1889521d5fb25adddc385a56b53d978d6cdd
dca123 <dsenanayaka1@gmail.com>
2015-03-12 15:54:48 +0530

    Added Specific Commitee Page

--- js/comm.js
+++ js/comm.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-	$('.ga1').hoverIntent(function () {
+	$('.wrapper').hoverIntent(function () {
 		$(this).removeClass("unzoom");
 		$(this).addClass("zoom");
-		$(this).children(".title").fadeOut(500);
-		$(this).children(".sub-title").fadeOut(500, function () {
-			$(this).siblings("span").fadeIn(500);
-		});
+//		$(this).children(".title").fadeOut(500);
+//		$(this).children(".sub-title").fadeOut(500, function () {
+//			$(this).siblings("span").fadeIn(500);
+//		});
 	}, function () {
 		$(this).addClass("unzoom");
 		$(this).removeClass("zoom");
-		$(this).children("span").fadeOut(500, function () {
-			$(this).siblings(".title").fadeIn(500);
-			$(this).siblings(".sub-title").fadeIn(500);
-		});
+//		$(this).children("span").fadeOut(500, function () {
+//			$(this).siblings(".title").fadeIn(500);
+//			$(this).siblings(".sub-title").fadeIn(500);
+//		});
 
 	});
 });
